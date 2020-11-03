ddcd93e5fa8cfc223ab9d02e3319c4f5677bfc50
caverdude <javacaverdude@gmail.com>
2020-11-02 22:29:29 -0600

    Angular 9 with TypeScript Page 29

--- beginangular/src/app/app.component.ts
+++ beginangular/src/app/app.component.ts
@@ -3,9 +3,10 @@ import {ProductService} from "./product.service";
 
 @Component({
   selector: 'app-root',
-  template: `<h1>{{title}}</h1> <products></products>`,
+  template: `<h1>{{title}}</h1> <img src="{{imageUrl}}">`,
   providers: [ProductService]
 })
 export class AppComponent {
   title = 'My First Angular App!';
+  imageUrl = "https://www.w3schools.com/html/pic_mountain.jpg";
 }
