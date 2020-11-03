841eeca2233b36dc409776d8e2dc4fc7fc5c2a78
caverdude <javacaverdude@gmail.com>
2020-11-02 19:06:51 -0600

    Angular 9 with TypeScript

--- beginangular/src/app/app.component.ts
+++ beginangular/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>{{title}}</h1> <products></products>`
 })
 export class AppComponent {
-  title = 'beginangular';
+  title = 'My First Angular App!';
 }
