b16cfb7e53e32f2d0fc6ae80a3b7350a4de94052
caverdude <javacaverdude@gmail.com>
2020-11-03 16:37:53 -0600

    Angular 9 with TypeScript Chap 6 Page 67

--- beginangular2/src/app/app.module.ts
+++ beginangular2/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { UserFormComponent} from "./user-form.component";
 
+
 @NgModule({
   declarations: [
     AppComponent, UserFormComponent
