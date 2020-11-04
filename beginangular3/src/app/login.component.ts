b3d81614b54defdf33813f5d22df72c326f8a883
caverdude <javacaverdude@gmail.com>
2020-11-04 00:09:05 -0600

    Angular 9 with TypeScript Chap 7 Page 81

--- beginangular3/src/app/login.component.ts
+++ beginangular3/src/app/login.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector:'login',
   templateUrl: 'login.component.html'
 })
 export class LoginComponent{
-  form = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
-  });
+  form: FormGroup
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username:['',Validators.required],
+      password:['',Validators.required]
+    })
+  }
   login(){
     console.log(this.form.value); // prints form values in json format
   }
