65008a18a345dd9f24b6de4b9805c85ae6c5e3b3
caverdude <javacaverdude@gmail.com>
2020-11-05 01:37:51 -0600

    Angular 9 with TypeScript Chap 10 Page 120

--- beginangular6/src/app/app.component.ts
+++ beginangular6/src/app/app.component.ts
@@ -7,10 +7,18 @@ import { FormControl } from "@angular/forms";
 @Component({
   selector: 'app-root',
   template: `
-    <ul>
-      <li><a routerLink="">Home</a> </li>
-      <li><a routerLink="GitHub">GitHub</a> </li>
-    </ul>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" routerLink="">Home<span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" routerLink="GitHub">GitHub<span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
     <router-outlet></router-outlet>
   `,
   providers: []
