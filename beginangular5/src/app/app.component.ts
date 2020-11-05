88bd2ef83db04e7e63d2fdcc32d7bee6b9e223e4
caverdude <javacaverdude@gmail.com>
2020-11-04 21:55:25 -0600

    Angular 9 with TypeScript Chap 9 Page 108

--- beginangular5/src/app/app.component.ts
+++ beginangular5/src/app/app.component.ts
@@ -3,12 +3,32 @@ import { GitHubService} from "./github.service";
 
 @Component({
   selector: 'app-root',
-  template: ` <div *ngIf="isLoading">Getting data...</div>
+  template: `
+  <div *ngIf="isLoading">
+    <i class="fa fa-spinner fa-spin fa-3x"></i>
+  </div>
+  <h3>GitHub User Results</h3>
+  <div *ngIf="isLoading">
+    <i class="fa fa-spinner fa-spin fa-3x"></i>
+  </div>
+  <div *ngFor="let user of users" class="media">
+  <div class="media">
+    <a href="{{ user.html_url }}">
+       <img src="{{ user.avatar_url }}" class="mr-4" alt="..." width="64" height="64">
+    </a>
+
+    <div class="media-body">
+      <h5 class="mt-0">{{user.login }}</h5>
+      Score: {{ user.score }}
+    </div>
+  </div>
+  </div>
   `,
   providers: [GitHubService]
 })
 export class AppComponent {
   isLoading = true;
+  users = [];
   constructor(private _githubService: GitHubService) {
 
   }
@@ -16,7 +36,8 @@ export class AppComponent {
     this._githubService.getGitHubData('greg')
       .subscribe(data => {
         this.isLoading=false;
-        console.log(data);
+        this.users=data.items;
+        console.log(data.items);
       });
   }
 }
