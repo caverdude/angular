2c0e070bb27dbf79e1eb28d3feecd4334d679fee
caverdude <javacaverdude@gmail.com>
2020-11-05 20:07:37 -0600

    Angular 9 with TypeScript Chapter 12 Page 152

--- AngularCRUD/src/app/app.component.ts
+++ AngularCRUD/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello</h1>`
 })
 export class AppComponent {
-  title = 'AngularCRUD';
+  constructor(private afs: AngularFirestore ){
+    console.log(afs);
+  }
 }
--- AngularCRUD/src/environments/environment.ts
+++ AngularCRUD/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAFYJfwFP8C1o2T9VUQim66GL4diJ9tN54",
+    authDomain: "angularexample-61346.firebaseapp.com",
+    databaseURL: "https://angularexample-61346.firebaseio.com",
+    projectId: "angularexample-61346",
+    storageBucket: "angularexample-61346.appspot.com",
+    messagingSenderId: "365040967802",
+    appId: "1:365040967802:web:62c73c8a034e15225f5bc3",
+    measurementId: "G-6F1RH6BT7E"
+  }
 };
 
 /*
